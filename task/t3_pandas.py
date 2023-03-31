8e1b04753781305ed56e03b2785be3e57ea4ad3d
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-03-31 20:08:49 +0500

    update code

--- task/t3_pandas.py
+++ task/t3_pandas.py
@@ -5,6 +5,7 @@ file_path = "f2.csv"
 data_frame = pd.read_csv(file_path)
 
 event_list = data_frame[' Events']
-rain_dates = data_frame[event_list.isin(['Rain', 'Snow', 'Rain-Snow'])]
- 
-print(rain_dates['PKT'])
+event_dates_bol = data_frame[event_list.isin(['Rain', 'Snow', 'Rain-Snow'])]
+event_dates = event_dates_bol['PKT']
+
+print(event_dates)
