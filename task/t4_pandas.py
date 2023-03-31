d7283e35c36e320d88bf3ed74c67a558c2cab37c
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-03-31 23:46:55 +0500

    weather pandas

--- task/t4_pandas.py
+++ task/t4_pandas.py
@@ -5,8 +5,9 @@ file_path = "f2.csv"
 data_frame = pd.read_csv(file_path)
 
 event_list = data_frame[' Events']
-rain_dates = data_frame[event_list.isin(['Thunderstorm'])]
-date = pd.to_datetime(rain_dates['PKT'])
+data_thunderstorm = data_frame[event_list.isin(['Thunderstorm'])]
+date = pd.to_datetime(data_thunderstorm['PKT'])
 week_days = date.dt.strftime('%A')
 
 print(week_days)
+
